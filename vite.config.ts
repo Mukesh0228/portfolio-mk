c9b2405dd92064ea6283f846206685a4934ea433
MukeshKumar M <mukeshk7660@gmail.com>
2026-06-04 17:34:25 +0530

    fix netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio-mk/"
+  plugins: [react()]
 });
